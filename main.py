3261e58f799c9eca1d1447e61141b4d380a33049
Anjali <choubeyanjali53@gmail.com>
2024-05-25 19:09:54 +0530

    created a snake class and handling snake movement through key press

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
-
+screen.tracer(0)
 screen.setup(height=600, width=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+screen.listen()
+snake = Snake()
 
-for position in STARTING_POSITION:
-    new_segment = Turtle()
-    new_segment.shape("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-
-
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
